0e82f5a8d45c09c48cd29917e0878b1a7391bb8e
Mario Sasko <mario.sasko@fer.hr>
2018-06-30 01:59:24 +0200

    Lines indented properly

--- zbroji.py
+++ zbroji.py
@@ -4,8 +4,8 @@
 def decorator(func):
 	def wrapper(x, y):
 		print(f'Result is: {func(x, y)}')
-
 	return wrapper
+
 @decorator
 def mnozi(x, y):
 	return x * y
@@ -15,5 +15,5 @@ zbroji = lambda x, y: x + y
 oduzmi = lambda x, y: x - y
 
 if __name__ == '__main__':
-	print(f'Zbroj: {zbroji(12, 10)}, razlika: {oduzmi(21, 12)},')
+	print(f'Zbroj: {zbroji(12, 10)}, razlika: {oduzmi(21, 12)}')
 	mnozi(32, 100)
