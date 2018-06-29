d98623ecd63d7dfbe2f14ccbb1db96940b31bc8b
Mario Sasko <mario.sasko@fer.hr>
2018-06-30 01:53:20 +0200

    Added decorator

--- zbroji.py
+++ zbroji.py
@@ -1,11 +1,19 @@
 # Simple python file
 
+# New branch, adding decorator 
+def decorator(func):
+	def wrapper(x, y):
+		print(f'Result is: {func(x, y)}')
+
+	return wrapper
+@decorator
+def mnozi(x, y):
+	return x * y
+
+
 zbroji = lambda x, y: x + y
 oduzmi = lambda x, y: x - y
 
-# New branch
-divide = lambda x, y: x / y
-
 if __name__ == '__main__':
 	print(f'Zbroj: {zbroji(12, 10)}, razlika: {oduzmi(21, 12)},')
-	
+	mnozi(32, 100)
