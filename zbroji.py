2ab45c8139faaf08c0cbe344d1b4624689c887c8
Mario Sasko <mario.sasko@fer.hr>
2018-06-30 01:51:45 +0200

    Added decorator

--- zbroji.py
+++ zbroji.py
@@ -3,6 +3,9 @@
 zbroji = lambda x, y: x + y
 oduzmi = lambda x, y: x - y
 
+# New branch
+divide = lambda x, y: x / y
+
 if __name__ == '__main__':
-	print(f'Zbroj: {zbroji(12, 10)}, razlika: {oduzmi(21, 12)}')
+	print(f'Zbroj: {zbroji(12, 10)}, razlika: {oduzmi(21, 12)},')
 	
