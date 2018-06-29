2f568018421c3384a1d18c8f1be1371d7ddf21ad
Mario Sasko <mario.sasko@fer.hr>
2018-06-30 01:38:12 +0200

    Added function oduzmi

--- zbroji.py
+++ zbroji.py
@@ -1,7 +1,8 @@
 # Simple python file
 
 zbroji = lambda x, y: x + y
+oduzmi = lambda x, y: x - y
 
 if __name__ == '__main__':
-	zbroji(12, 10)
+	print(f'Zbroj: {zbroji(12, 10)}, razlika: {oduzmi(21, 12)}')
 	
